50091a26be6c4dc85384aea698b3c3fa004a876c
Carlitos Andrade <carlos09042003c@gmail.com>
2026-03-11 15:57:25 -0500

    corregida la ruta del login

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function App() {
                     />
 
                     <Route
-                        path="./login"
+                        path="/login"
                         element={
                             <Login setCurrentUser={setCurrentUser} />
                         }
